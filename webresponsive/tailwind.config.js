63dec8741be6518889b43517020c75db86b7d822
Mluthfirizky <luthfi.ginting@icloud.com>
2021-08-10 15:00:58 +0700

    Login-page

---
+++ webresponsive/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './*.html' //dia akan mendeteksi semua file yg berjenis html
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
